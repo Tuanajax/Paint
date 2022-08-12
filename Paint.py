bfc2a3909b9cc54d2c45238acfaf55f6851e875e
tuanvu <vutuan190294@gmail.com>
2022-08-12 16:01:49 +0700

    6st

--- Paint.py
+++ Paint.py
@@ -4,12 +4,3 @@ Tuan_br = turtle.Turtle()
 print("111")
 print("github")
 print("git hub update")
-<<<<<<< HEAD
-Print("git hut 2")
-print("i am git hub")
-=======
-print("git hut 2")
-print("11111")
-
->>>>>>> 3a58959 (5st)
-
